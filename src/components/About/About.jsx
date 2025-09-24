01e01e976f7942d0aaae52dcfb3fddc6d4149acc
Soumil Mehta <mehtasoumil2407@gmail.com>
2025-09-24 20:56:35 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1-3uvHHAAT0Sswf8Dh5ZT2AOUCirR-GAK/view?pli=1"
+            href="https://drive.google.com/file/d/1bMGs8grIt-lIuA8CZrdp54esP5ncDdNi/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
